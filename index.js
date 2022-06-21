146496e16f61a4481e019ddebe323d579be45829
The Nithin Balaji <thenithinbalaji@gmail.com>
2022-06-21 19:11:09 +0530

    removed dm commands (#2)
    removed dm commands from index.js, fixed missing packages

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ client.registry
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  console.log(`Logged in as ${client.user.tag}!`);
   client.user.setActivity('"!help" - for help');
 });
 
@@ -42,39 +41,6 @@ client.on("messageCreate", msg => {
   if(msg.author.bot)
     return
 
-  else
-  {
-    
-    if(msg.channel.type == "DM"){
-
-      if (msg.content == dm_prefix + " " + "help") {
-        const helpEmbed = new MessageEmbed()
-        .setTitle('DM help')
-        .addField('🖥️ Server Suggestions', `${dm_prefix} suggest <message>`, false)
-        .setFooter('Want to suggest something? DM this bot');
-        msg.channel.send({ embeds: [helpEmbed] });
-      }
-  
-      else if(msg.content.startsWith(`${dm_prefix} suggest`)){
-        info = msg.content.slice((`${dm_prefix} suggest`).length)
-  
-        if (info.length > 5){
-        const suggestEmbed = new MessageEmbed()
-        .setTitle('Suggestion')
-        .setDescription(info)
-
-        msg.client.channels.cache.get("980040052816113665").send({ embeds: [suggestEmbed] }) 
-        // get channel id this from db!!!
-        msg.react('✅');
-
-        }
-      }
-  
-      else {
-        msg.author.send(`The DM prefix is -> \`${dm_prefix}\` \nTry typing \`${dm_prefix} help\` `)
-      }
-    }
-  }
 })
 
 // Handle all bot functions invoked via slash commands here:
